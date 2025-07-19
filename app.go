4c8a28b3e9aab7ffdabb6ebb3a4fc19d3a2b1523
DuongNT <DuongNT@splus-software.com.vn>
2025-07-19 12:53:51 +0700

    feat: Add MySQL database connection and configuration, update frontend dependencies

--- app.go
+++ app.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+	db  *sql.DB
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	_ = godotenv.Load()
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, dbname)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to MySQL: %v", err))
+	}
+	return &App{db: db}
 }
 
 // startup is called when the app starts. The context is saved
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module redmine-go-app
 
 go 1.23
 
-require github.com/wailsapp/wails/v2 v2.10.2
+require (
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/joho/godotenv v1.5.1
+	github.com/wailsapp/wails/v2 v2.10.2
+)
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
